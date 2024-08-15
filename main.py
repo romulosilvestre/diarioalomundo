a3229cd9b1515630134369746478ec5d3f907f9f
= <=>
2024-08-15 17:59:24 -0300

    ok

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
 from flask import Flask, render_template,request
-from markupsafe import escape
+# Importa a função `sessionmaker`, que é usada para criar uma nova sessão para interagir com o banco de dados
+from sqlalchemy.orm import sessionmaker
+
+# Importa as funções `create_engine` para estabelecer uma conexão com o banco de dados e `MetaData` para trabalhar com metadados do banco de dados
+from sqlalchemy import create_engine, MetaData
+
+# Importa a função `automap_base`, que é usada para refletir um banco de dados existente em classes ORM automaticamente
+from sqlalchemy.ext.automap import automap_base
+from aluno import Aluno
 
 app = Flask(__name__)
+
+# Criando a configuração do banco de dados
+# Configuração do Banco de Dados
+# biblioteca para converter e resolver problema do @
+import urllib.parse
+
+# Qual o usuário do banco e a senha?
+
+user = 'root'
+password = urllib.parse.quote_plus('senai@123')
+
+host = 'localhost'
+database = 'projetodiario1'
+connection_string = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+
+# Criar a engine e refletir o banco de dados existente
+engine = create_engine(connection_string)
+metadata = MetaData()
+metadata.reflect(engine)
+
+# Mapeamento automático das tabelas para classes Python
+Base = automap_base(metadata=metadata)
+Base.prepare()
+
+# Acessando a tabela 'aluno' mapeada
+Aluno = Base.classes.aluno
+
+
+
+# Criar a sessão do SQLAlchemy
+Session = sessionmaker(bind=engine)
+session = Session()
+
 
 @app.route("/")
 def index():
@@ -17,6 +58,22 @@ def inserir_aluno():
     nome = request.form['nome']
     tempoestudo = request.form['tempoestudo']
     rendafamiliar = request.form['rendafamiliar']
+     
+    #sessão ok
+    aluno = Aluno(ra=ra,nome=nome,tempoestudo=tempoestudo,rendafamiliar=rendafamiliar)
+
+    try:
+       session.add(aluno)
+       session.commit() 
+    except:
+       session.rollback() 
+    finally:
+       session.close()
+    mensagem = "cadastrado com sucesso"
+    return render_template('index.html',mensagem=mensagem)
+
+
+
     return render_template('index.html',ra=ra)
 
 if __name__ == "__main__":
